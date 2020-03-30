30ecc25200fea209432ec5ca3954316860db095e
emreceran <emrecrn@gmail.com>
2020-03-30 20:34:06 +0300

    Veritabanından gönderileri göstermek için template'ler değiştirildi.

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 from .models import Post
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 
 def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    #mecburi yorum
+    ben = User.objects.get(username='emre')
+
+    posts = Post.objects.filter(author=ben).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
