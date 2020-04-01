4d2ba4208a7b6d57d0bca7aeeacd92ee73a92172
emreceran <emrecrn@gmail.com>
2020-04-02 00:31:19 +0300

    biseyler oldu en son deneme

--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,7 @@
 from django.shortcuts import render
-from .models import Post
 from django.utils import timezone
-from django.contrib.auth.models import User
-
+from .models import Post
 
 def post_list(request):
-    #mecburi yorum
-    ben = User.objects.get(username='emre')
-
-    posts = Post.objects.filter(author=ben).order_by('published_date')
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
